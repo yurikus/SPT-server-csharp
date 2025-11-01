fb568da966607c7025141c6db43179ac825328af
Archangel <jesse@archangel.wtf>
2025-11-01 14:08:26 +0100

    Fix up mock

--- Testing/UnitTests/Mock/MockRandomUtil.cs
+++ Testing/UnitTests/Mock/MockRandomUtil.cs
@@ -8,45 +8,45 @@ namespace UnitTests.Mock;
 [Injectable(TypeOverride = typeof(RandomUtil))]
 public class MockRandomUtil(ISptLogger<RandomUtil> logger, ICloner cloner) : RandomUtil(logger, cloner)
 {
-    public override int GetInt(int min, int max = int.MaxValue, bool exclusive = false)
+    public new int GetInt(int min, int max = int.MaxValue, bool exclusive = false)
     {
         return min;
     }
 
-    public override double GetDouble(double min, double max)
+    public new double GetDouble(double min, double max)
     {
         return min;
     }
 
-    public override bool GetBool()
+    public new bool GetBool()
     {
         return true;
     }
 
-    public override void NextBytes(Span<byte> bytes)
+    public new void NextBytes(Span<byte> bytes)
     {
         // TODO: No idea what this does
         base.NextBytes(bytes);
     }
 
-    public override double GetPercentOfValue(double percent, double number, int toFixed = 2)
+    public new double GetPercentOfValue(double percent, double number, int toFixed = 2)
     {
         // TODO: No idea what this does
         return base.GetPercentOfValue(percent, number, toFixed);
     }
 
-    public override double ReduceValueByPercent(double number, double percentage)
+    public new double ReduceValueByPercent(double number, double percentage)
     {
         // TODO: No idea what this does
         return base.ReduceValueByPercent(number, percentage);
     }
 
-    public override bool GetChance100(double? chancePercent)
+    public new bool GetChance100(double? chancePercent)
     {
         return true;
     }
 
-    public override T GetRandomElement<T>(IEnumerable<T> collection)
+    public new T GetRandomElement<T>(IEnumerable<T> collection)
     {
         if (!collection.Any())
         {
@@ -56,66 +56,65 @@ public class MockRandomUtil(ISptLogger<RandomUtil> logger, ICloner cloner) : Ran
         return collection.First();
     }
 
-    public override TKey GetKey<TKey, TVal>(Dictionary<TKey, TVal> dictionary)
+    public new TKey GetKey<TKey, TVal>(Dictionary<TKey, TVal> dictionary)
     {
         return GetRandomElement(dictionary.Keys);
     }
 
-    public override TVal GetVal<TKey, TVal>(Dictionary<TKey, TVal> dictionary)
+    public new TVal GetVal<TKey, TVal>(Dictionary<TKey, TVal> dictionary)
     {
         return GetRandomElement(dictionary.Values);
     }
 
-    public override double GetNormallyDistributedRandomNumber(double mean, double sigma, int attempt = 0)
+    public new double GetNormallyDistributedRandomNumber(double mean, double sigma, int attempt = 0)
     {
         // TODO: No idea what to do with this
         return base.GetNormallyDistributedRandomNumber(mean, sigma, attempt);
     }
 
-    public override int RandInt(int low, int? high = null)
+    public new int RandInt(int low, int? high = null)
     {
         return low;
     }
 
-    public override double RandNum(double val1, double val2 = 0, int precision = 6)
+    public new double RandNum(double val1, double val2 = 0, int precision = 6)
     {
         return val1;
     }
 
-    public override List<T> DrawRandomFromList<T>(List<T> originalList, int count = 1, bool replacement = true)
+    public new List<T> DrawRandomFromList<T>(List<T> originalList, int count = 1, bool replacement = true)
     {
         return originalList.Slice(0, count);
     }
 
-    public override List<TKey> DrawRandomFromDict<TKey, TVal>(Dictionary<TKey, TVal> dict, int count = 1, bool replacement = true)
+    public new List<TKey> DrawRandomFromDict<TKey, TVal>(Dictionary<TKey, TVal> dict, int count = 1, bool replacement = true)
     {
         // TODO: derandomize
         return base.DrawRandomFromDict(dict, count, replacement);
     }
 
-    public override double GetBiasedRandomNumber(double min, double max, double shift, double n)
+    public new double GetBiasedRandomNumber(double min, double max, double shift, double n)
     {
         return min;
     }
 
-    public override List<T> Shuffle<T>(List<T> originalList)
+    public new List<T> Shuffle<T>(List<T> originalList)
     {
         return originalList;
     }
 
-    public override int GetNumberPrecision(double num)
+    public new int GetNumberPrecision(double num)
     {
         // TODO: derandomize
         return base.GetNumberPrecision(num);
     }
 
-    public override T? GetArrayValue<T>(IEnumerable<T> list)
-        where T : default
+    public new T? GetArrayValue<T>(IEnumerable<T> list)
     {
         return GetRandomElement(list);
     }
 
-    public override bool RollChance(double chance, double scale = 1)
+    public new bool RollChance(double chance, double scale = 1)
     {
         return true;
     }
