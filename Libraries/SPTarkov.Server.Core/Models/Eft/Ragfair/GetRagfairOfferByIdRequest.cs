1439ddcb11939e1d575d0d528e3dca0c6baa661b
Chomp <dev@dev.sp-tarkov.com>
2025-12-26 13:16:38 +0000

    Fixed `client/ragfair/offer/findbyid` endpoint failure

--- Libraries/SPTarkov.Server.Core/Models/Eft/Ragfair/GetRagfairOfferByIdRequest.cs
+++ Libraries/SPTarkov.Server.Core/Models/Eft/Ragfair/GetRagfairOfferByIdRequest.cs
@@ -1,8 +1,9 @@
 using System.Text.Json.Serialization;
+using SPTarkov.Server.Core.Models.Utils;
 
 namespace SPTarkov.Server.Core.Models.Eft.Ragfair;
 
-public record GetRagfairOfferByIdRequest
+public record GetRagfairOfferByIdRequest : IRequestData
 {
     [JsonPropertyName("id")]
     public int? Id { get; set; }
